613ed298f1129eb4482a06d402c47b36742e3dac
Learning D3 <learningd3js@gmail.com>
2018-02-05 20:28:29 -0800

    Adding basic version of the prototype

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import logo from './logo.svg';
+import Search from './Search';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="nav">
+          <img src={logo} className="nav-logo" />
+          <div className="nav-browse">
+          </div>
+          <Search />
+        </div>
+
+        <div className="container">
+          <h2 className="tc mt4">Psalms 1</h2>
+          <div className="mt3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div className="mt2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div className="mt2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div className="mt2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div className="mt2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+          <div className="mt2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Search.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { HotKeys } from 'react-hotkeys';
+import "./search.css";
+
+const map = {
+  'search': 'command+s',
+  'down': 'down'
+};
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDropdown: false,
+      filteredList: [],
+      list: [
+        "Matt 1",
+        "Matt 2",
+        "Mark 1",
+        "Luke 1",
+        "John 1"
+      ]
+
+    };
+
+    this.toggleDropdown = this.toggleDropdown.bind(this)
+    this.filterList = this.filterList.bind(this)
+  }
+
+  filterList(event) {
+    var updatedList = this.state.list;
+    updatedList = updatedList.filter(function(item){
+      return item.toLowerCase().search(
+        event.target.value.toLowerCase()) !== -1;
+    });
+
+    this.setState({filteredList: updatedList});
+  }
+
+  toggleDropdown() {
+    this.setState(previousState => {
+      return { showDropdown: !previousState.showDropdown };
+    });
+  }
+
+
+  render() {
+    return (
+        <div className="search-container">
+          <input
+            onFocus={this.toggleDropdown}
+            onBlur={this.toggleDropdown}
+            placeholder="Search"
+            className="input"
+            onChange={this.filterList}
+          />
+          <div className="search-dropdown">
+            {this.state.filteredList.map(function(d, id) {
+              return (
+                <div key={id} className="search-dropdown-item"> {d} </div>
+              )
+            })}
+          </div>
+        </div>
+
+    )
+  }
+}
+
+export default Search;
